f578dc0f6f63e5911c19485c03226b112d14875d
misr-company-ai <omarmxndncbbf@gmail.com>
2026-07-26 20:46:39 +0300

    Add MessagesFragment class for message display

---
+++ app/src/main/java/com/example/messagesup/fragments/MessagesFragment.kt
@@ -0,0 +1,14 @@
+package com.example.messagesup.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.messagesup.R
+
+class MessagesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_messages, container, false)
+    }
+}
